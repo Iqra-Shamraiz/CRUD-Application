126bf653df0dd9b0834a47066827083a7862ab97
Iqra Shamraiz <Learning4402823@gmail.com>
2024-11-02 03:37:41 -0700

    Add files via upload

---
+++ StudentController.cs
@@ -0,0 +1,52 @@
+﻿using CurdAPPUsingApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace CurdAPPUsingApi.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly string url = "http://localhost:5119/api/StudentAPI/";
+        private readonly HttpClient client = new HttpClient();
+
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                students = JsonConvert.DeserializeObject<List<Student>>(result);
+            }
+            return View(students);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(student);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("", "Error creating student.");
+            }
+            return View(student);
+        }
+    }
+}
